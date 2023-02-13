d56ce10f78b6bc56a58672655de6ee659381f643
andreas <andreas@wellenvogel.de>
2023-02-13 19:42:30 +0100

    add plugin info and disabled state to package list

--- server/packagelist.py
+++ server/packagelist.py
@@ -25,6 +25,7 @@
 import subprocess
 import logging
 import re
+import os
 
 
 
@@ -33,6 +34,7 @@ class NV:
     self.version=None
     self.candidate=None
     self.state=None
+    self.disabled=False
     for k,v in kwargs.items():
       self.__setattr__(k,v)
   def set(self,n,v):
@@ -58,6 +60,51 @@ class PackageList:
     "version":"^  *Installed: *",
     "candidate":"^  *Candidate: *"
   }
+
+  def piNameToVar(self,name):
+    try:
+      return re.sub("[^0-9a-zA-Z]","",name).upper()
+    except:
+      return name
+
+  def getDisabledPackages(self):
+    script=os.path.abspath(os.path.join(os.path.dirname(__file__),os.pardir,os.pardir,os.pardir,"plugin.sh"))
+    logging.debug("scriptPath=%s",script)
+    if not os.path.exists(script):
+      return []
+    res=subprocess.run([script,"list"],capture_output=True,shell=True)
+    if res.returncode != 0:
+      logging.error("execution of %s returned %d"%(script,res.returncode))
+      return []
+    plugins=set()
+    for line in res.stdout.splitlines(): 
+      line=line.decode("utf-8",errors="ignore")
+      parts=line.split("=")
+      if len(parts) != 2:
+        continue
+      if parts[1].rstrip().lstrip() == "1":
+        plugins.add(parts[0].rstrip())
+    if len(plugins) < 1:
+      return []
+    cmd=["dpkg-query","-W","-f", "${Package}:${avnav-plugin}\n", self.prefix+"*"]
+    res=subprocess.run(cmd,capture_output=True)
+    if res.returncode != 0:
+      logging.error("execution of %s returned %d"%(" ".join(cmd),res.returncode))
+      return []
+    rt=[]  
+    for line in res.stdout.splitlines(): 
+      line=line.decode("utf-8",errors="ignore")
+      parts=line.split(":")
+      if len(parts) != 2:
+        continue 
+      logging.debug("package with plugin description: %s"%line)
+      varName=self.piNameToVar(parts[1])
+      if varName in plugins:
+        rt.append(parts[0])  
+    logging.debug("disabled packages=%s"%",".join(rt))    
+    return rt    
+
+    
   def fetchPackages(self):
     CMD=["apt-cache","policy",self.prefix+"*"]
     res=subprocess.run(CMD,capture_output=True,timeout=10)
@@ -82,6 +129,7 @@ class PackageList:
             pkgData.set(key,v)
     if pkg is not None:
       rt[pkg]=pkgData
+    disabled=self.getDisabledPackages()  
     for pkg in rt.keys():
       version=rt[pkg].version
       if version is None or version == "(none)":
@@ -90,7 +138,8 @@ class PackageList:
       else:
         rt[pkg].state="installed"
       if rt[pkg].version == rt[pkg].candidate:
-        rt[pkg].candidate=None  
+        rt[pkg].candidate=None
+      rt[pkg].disabled=True if pkg in disabled else False    
     rtlist=[]
     for k,pkg in rt.items():
       if self.installedOnlyPrefix is not None and k.startswith(self.installedOnlyPrefix):
